17eeccc82e33b6677823a98aac10a5fb9cd24c03
Ubuntu <muefiloglu@protonmail.com>
2020-10-30 13:33:26 +0000

    Message Output

--- app.js
+++ app.js
@@ -62,5 +62,14 @@ startGameBtn.addEventListener('click', () => {
     const playerSelection   = getPlayerChoice();
     const computerSelection = getComputerChoice();
     const winner = getWinner(computerSelection, playerSelection);
-    console.log(winner);
+    let message = `You picked ${playerSelection}, computer picked ${computerSelection} therefore you `;
+    if (winner === RESULT_DRAW) {
+        message = message + 'had a draw.';
+    } else if (winner === RESULT_PLAYER_WINS) {
+        message = message + 'won.';
+    } else {
+        message = message + 'lost.';
+    }
+    alert(message);
+    gameIsRunning = false;
 });
